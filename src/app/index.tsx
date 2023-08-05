4b32df162fc90159399c71d6dff46e93a823bd5d
Marcelio Santana <marcelio.santana@riachuelo.com.br>
2023-08-04 22:58:19 -0300

    feat(0003) configure Tailwind CSS e add expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+
+import { Link } from 'expo-router';
+
+
+export default function Page() {
+  return (
+    <View>
+      <Link href="/about">About</Link>
+    </View>
+  );
+}
