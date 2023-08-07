4e7cbadd505bbc371661230578f7f06a6c73a46f
Marcelio Santana <marcelio.santana@riachuelo.com.br>
2023-08-07 17:45:46 -0300

    feat(0002) centralizar o link de navegacao

--- app/about.tsx
+++ app/about.tsx
@@ -4,7 +4,7 @@ import { Link } from "expo-router";
 export default function Page() {
   return (
     <Link href="/" asChild>
-      <Pressable>
+      <Pressable className="flex-1 items-center justify-center bg-white">
         <Text>Home</Text>
       </Pressable>
     </Link>
