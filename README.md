598e53082dddab623e83832cf2e28cf663ae8646
Marcelio Santana <marcelio.santana@riachuelo.com.br>
2023-09-29 11:24:08 -0300

    feat(0001) hello world theme customized nextUI

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
-# Basic API routes example
+# Next.js & NextUI Template
 
-Next.js ships with [API routes](https://nextjs.org/docs/api-routes/introduction) which provides an easy solution to build your own `API`. This example shows how to create multiple `API` endpoints with serverless functions, which can execute independently.
+This is a template for creating applications using Next.js 13 (pages directory) and NextUI (v2).
 
-## Deploy your own
+## Technologies Used
 
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/api-routes)
+- [Next.js 13](https://nextjs.org/docs/getting-started)
+- [NextUI](https://nextui.org)
+- [Tailwind CSS](https://tailwindcss.com)
+- [Tailwind Variants](https://tailwind-variants.org)
+- [TypeScript](https://www.typescriptlang.org)
+- [Framer Motion](https://www.framer.com/motion)
+- [next-themes](https://github.com/pacocoursey/next-themes)
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/vercel/next.js/tree/canary/examples/api-routes&project-name=api-routes&repository-name=api-routes)
+## How to Use
 
-## How to use
-
-Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), or [pnpm](https://pnpm.io) to bootstrap the example:
-
-```bash
-npx create-next-app --example api-routes api-routes-app
-```
-
-```bash
-yarn create next-app --example api-routes api-routes-app
-```
+To create a new project based on this template using `create-next-app`, run the following command:
 
 ```bash
-pnpm create next-app --example api-routes api-routes-app
+npx create-next-app -e https://github.com/nextui-org/next-pages-template
 ```
+## License
 
-Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+Licensed under the [MIT license](https://github.com/nextui-org/next-pages-template/blob/main/LICENSE).
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,88 @@
-import useSWR from 'swr'
-import PersonComponent from '../components/Person'
-import type { Person } from '../interfaces'
+import NextLink from "next/link";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+import { Button } from "@nextui-org/react";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+export default function IndexPage() {
+	return (
+		<DefaultLayout>
+			<h1 className="text-3xl font-bold underline">
+				Hello world!
+			</h1>
+			<div className="flex flex-wrap gap-4 items-center">
+				<Button color="primary" variant="solid">
+					Solid
+				</Button>
+				<Button color="primary" variant="faded">
+					Faded
+				</Button>
+				<Button color="primary" variant="bordered">
+					Bordered
+				</Button>
+				<Button color="primary" variant="light">
+					Light
+				</Button>
+				<Button color="primary" variant="flat">
+					Flat
+				</Button>
+				<Button color="primary" variant="ghost">
+					Ghost
+				</Button>
+				<Button color="primary" variant="shadow">
+					Shadow
+				</Button>
+			</div>
+			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+				<div className="inline-block max-w-lg text-center justify-center">
+					<h1 className={title()}>Make&nbsp;</h1>
+					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+					<br />
+					<h1 className={title()}>
+						websites regardless of your design experience.
+					</h1>
+					<h4 className={subtitle({ class: "mt-4" })}>
+						Beautiful, fast and modern React UI library.
+					</h4>
+				</div>
 
-export default function Index() {
-  const { data, error, isLoading } = useSWR<Person[]>('/api/people', fetcher)
+				<div className="flex gap-3">
+					<Link
+						isExternal
+						as={NextLink}
+						href={siteConfig.links.docs}
+						className={buttonStyles({
+							color: "primary",
+							radius: "full",
+							variant: "shadow",
+						})}
+					>
+						Documentation
+					</Link>
+					<Link
+						isExternal
+						as={NextLink}
+						className={buttonStyles({ variant: "bordered", radius: "full" })}
+						href={siteConfig.links.github}
+					>
+						<GithubIcon size={20} />
+						GitHub
+					</Link>
+				</div>
 
-  if (error) return <div>Failed to load</div>
-  if (isLoading) return <div>Loading...</div>
-  if (!data) return null
-
-  return (
-    <ul>
-      {data.map((p) => (
-        <PersonComponent key={p.id} person={p} />
-      ))}
-    </ul>
-  )
+				<div className="mt-8">
+					<Snippet hideSymbol hideCopyButton variant="bordered">
+						<span>
+							Get started by editing <Code color="primary">pages/index.tsx</Code>
+						</span>
+					</Snippet>
+				</div>
+			</section>
+		</DefaultLayout>
+	);
 }
